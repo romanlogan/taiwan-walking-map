fa4af8ac539877bccf0c3aa19173f0aec0a67ae0
romanlogal <dlqudals798@naver.com>
2024-01-20 22:52:14 +0800

    v2.21

---
+++ src/main/java/com/dbproject/web/plan/PlanController.java
@@ -0,0 +1,12 @@
+package com.dbproject.web.plan;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class PlanController {
+
+    @PostMapping
+}
