e7a6fd420aac00eb42329a31c314164186a5fd0e
lee798 <dlqudals798@naver.com>
2024-12-24 16:10:19 +0800

    new start

--- src/test/java/com/dbproject/controller/FavoriteControllerTest.java
+++ src/test/java/com/dbproject/controller/FavoriteControllerTest.java
@@ -32,7 +32,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = FavoriteController.class)
-class FavoriteControllerTest {
+class
+FavoriteControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
